69bfba1d728cc2b5b19a3275e205725bf1047f5d
Herman Ceaser <ceaserbanks09@gmail.com>
2020-10-06 18:29:49 +0300

    [update] setup ci-workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders welcome message", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = getByText(/welcome to plan it event planner/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test("renders app-logo description", () => {
+  const { getByAltText } = render(<App />);
+  const imgDescription = getByAltText(/logo/i);
+  expect(imgDescription).toBeInTheDocument();
 });
