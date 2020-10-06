362f4ed3d6ab33ab1386805a8fbb0799faf8036e
Herman Ceaser <ceaserbanks09@gmail.com>
2020-10-06 16:38:18 +0300

    [Added] added assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Plan it Event Planner</h1>
+        <p> How do you want your event planned?</p>
       </header>
     </div>
   );
